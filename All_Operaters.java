cb5c36b89555ded5c8879d802a56738dc071c400
Saikiranchagarla <chagarlasaikiran@gmail.com>
2025-03-10 18:22:48 +0530

    Add files via upload

---
+++ All_Operaters.java
@@ -0,0 +1,14 @@
+class All_Operaters 
+{
+	public static void main(String[] args) 
+	{
+		int a = 45;
+		int b = -45;
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a==b);
+		System.out.println(a!=b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+	}
+}
