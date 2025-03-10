e1b1e369050efe59266e03916694f2722bb0a3ba
Saikiranchagarla <chagarlasaikiran@gmail.com>
2025-03-10 23:56:07 +0530

    Add files via upload

---
+++ Factors_Of_Num.java
@@ -0,0 +1,16 @@
+class Factors_Of_Num 
+{
+	public static void main(String[] args) 
+	{
+		int a= 3;
+
+		if(10%a==0)
+		{
+			System.out.println(a + "is a factor of 10");
+		}
+		else
+		{
+			System.out.println(a + "is not a factor of 10");
+		}
+	}
+}
