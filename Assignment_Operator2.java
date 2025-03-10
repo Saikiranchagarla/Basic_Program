670bc6e5f92576f4b356262a0637fc9ff78ec0fa
Saikiranchagarla <chagarlasaikiran@gmail.com>
2025-03-10 23:53:54 +0530

    Add files via upload

---
+++ Assignment_Operator2.java
@@ -0,0 +1,13 @@
+class  Assignment_Operator2 
+{
+	public static void main(String[] args) 
+	{
+		int a = 14;
+		int b = 20;
+		int c = 3;
+		System.out.println(b+=2);
+		System.out.println(c-=9);
+		System.out.println(a++);
+		System.out.println(b--);
+	}
+}
