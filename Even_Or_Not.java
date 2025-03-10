4a51ed1e93230711d6a65e5e283b10cc921f3833
Saikiranchagarla <chagarlasaikiran@gmail.com>
2025-03-10 23:55:24 +0530

    Add files via upload

---
+++ Even_Or_Not.java
@@ -0,0 +1,10 @@
+class Even_Or_Not 
+{
+	public static void main(String[] args) 
+	{
+		int a = 50;
+		
+		String result = (a%2==0)?( a + " is a even number ") : ( a + "  is not a even number ");
+		System.out.println(result);
+	}
+}
