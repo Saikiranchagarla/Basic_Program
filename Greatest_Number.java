942efb77f44e5d87decc627a5f6ce69cf73e0c64
Saikiranchagarla <chagarlasaikiran@gmail.com>
2025-03-11 21:52:52 +0530

    Add files via upload

---
+++ Greatest_Number.java
@@ -0,0 +1,17 @@
+class Greatest_Number 
+{
+	public static void main(String[] args) 
+	{
+		int a = 15;
+		int b = 32;
+		
+		if(a>b)
+		{
+		System.out.println(a + "is not a biggest number");
+		}
+		else
+		{
+		System.out.println(b + "is a biggest number" );
+		}
+	}
+}
