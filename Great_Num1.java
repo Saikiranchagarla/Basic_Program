7f547200e1d40a6d1462c879de6fafd62a652ebf
Saikiranchagarla <chagarlasaikiran@gmail.com>
2025-03-11 21:51:34 +0530

    Add files via upload

---
+++ Great_Num1.java
@@ -0,0 +1,11 @@
+class Great_Num1 
+{
+	public static void main(String[] args) 
+	{
+		int a = 40;
+		int b = 300;
+		int result = (a>b)? a:b;
+		System.out.println(result + "is a greates number"
+);
+	}
+}
