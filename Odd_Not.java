aa6930a6282a7720d796c60f59d6093e06ab1b4b
Saikiranchagarla <chagarlasaikiran@gmail.com>
2025-03-13 16:30:27 +0530

    Add files via upload

---
+++ Odd_Not.java
@@ -0,0 +1,15 @@
+class Odd_Not 
+{
+	public static void main(String[] args) 
+	{
+		int a = 6;
+		if(a%2!=0)
+		{
+			System.out.println(a + " is a odd number");
+		}
+		else
+		{
+			System.out.println(a + " is not a odd number");
+		}
+	}
+}
