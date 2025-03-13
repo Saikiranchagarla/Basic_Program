c2c3dc167839c41274c77f15252dffb8ec0f3f1c
Saikiranchagarla <chagarlasaikiran@gmail.com>
2025-03-13 16:28:06 +0530

    Add files via upload

---
+++ Negative_not.java
@@ -0,0 +1,15 @@
+class Negative_not 
+{
+	public static void main(String[] args) 
+	{
+		int a = 100;
+		if (a>0)
+		{
+			System.out.println(a + "is a not a negative number");
+		}
+		else
+		{
+			System.out.println(a + "is a negative number");
+		}
+	}
+}
