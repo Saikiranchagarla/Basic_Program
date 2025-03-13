14fb895465889907262a18c233353f95d4f4dffa
Saikiranchagarla <chagarlasaikiran@gmail.com>
2025-03-13 16:29:34 +0530

    Add files via upload

---
+++ Smallest_Num.java
@@ -0,0 +1,10 @@
+class Smallest_Num 
+{
+	public static void main(String[] args) 
+	{
+		int a = 30;
+		int b = 45;
+		String result = (a<b) ? (a + "is a smallest number") : (a + "is not a smallest number");
+		System.out.println(result);
+	}
+}
