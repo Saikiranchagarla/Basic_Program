8d5135446efdb0c727f54ed9fb88325e44fb068d
Saikiranchagarla <chagarlasaikiran@gmail.com>
2025-03-20 19:28:59 +0530

    Add files via upload

---
+++ Numbers_Looping.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Numbers_Looping
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		for ( int i = 300; i>=250; i--)
+		System.out.println(i);
+	}
+}
