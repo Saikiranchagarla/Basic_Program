808f1b798b9108d3db1da0e0d2f64720f05c2ea6
Saikiranchagarla <chagarlasaikiran@gmail.com>
2025-03-20 19:11:49 +0530

    Add files via upload

---
+++ Alphabet_Lower.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Alphabet_Lower 
+{
+	public static void main(String[] args) 
+	{
+
+		Scanner sc = new Scanner(System.in);
+
+		for (char ch = 'a' ; ch<='z' ; ch++)
+		{
+			System.out.println("ch :" + ch );
+		}
+	}
+}
