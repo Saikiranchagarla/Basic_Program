2576ff0ceabd0ac48924d1ac638f85d77b6010ea
Saikiranchagarla <chagarlasaikiran@gmail.com>
2025-03-20 19:14:23 +0530

    Add files via upload

---
+++ Number_Sum.java
@@ -0,0 +1,17 @@
+class Number_Sum 
+{
+	public static void main(String[] args) 
+	{
+
+		int sum = 0;
+
+		for (int i =15; i<=30; i++)
+			{
+				sum = sum+i;
+
+			//	System.out.println("Sum of numbers  from 15 and 30 is " + sum);
+
+			}
+	    	System.out.println("Sum of numbers  from 15 and 30 is " + sum);
+	}
+}
