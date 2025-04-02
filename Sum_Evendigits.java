c82e90494a74adb2f7b5028ecd643e1fcb8888fe
Saikiranchagarla <chagarlasaikiran@gmail.com>
2025-04-02 23:37:07 +0530

    Add files via upload

---
+++ Sum_Evendigits.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Sum_Evendigits 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the value :");
+		int num = sc.nextInt();
+		int sum = 0;
+		while (num>0)
+		{
+			int dig = num%10;
+			if (dig%2==0)
+				{
+					sum = sum+dig;
+					//System.out.println( "sum of all the even digits of a number : " + sum );
+				}
+			num = num/10;
+		}
+		System.out.println( "sum of all the even digits of a number : " + sum );
+	}
+}
